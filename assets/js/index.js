cd810583e04b74abcd756eb6e244a5a5eef7aeca
ImElio <peruginoantonio573@gmail.com>
2025-03-02 06:56:58 +0100

    aggiunto salvataggio dal form, gestione tabs finita. Gestione dell'overlay caricamento. Caricamento comuni, inizio Generazione Codice Fiscale💪

--- assets/js/index.js
+++ assets/js/index.js
@@ -29,6 +29,47 @@ collegamentiNav.forEach((link) => {
 });
 
 /****************************/
-/* GESTIONE NAVBAR
+/* OVERLAY CARICAMENTO
 /****************************/
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("overlayCaricamento").classList.add("nascosto");
+    }, 1000);
+});
 
+/****************************/
+/* Caricamento COMUNI => comuni.json
+/****************************/
+const selectComune = document.getElementById("comune");
+fetch("comuni/comuni.json")
+.then((risposta) => risposta.json())
+.then((datiComuni) => {
+    datiComuni.forEach((comune) => {
+        const opzione = document.createElement("option");
+        opzione.value = comune.codice;
+        opzione.textContent = comune.nome;
+        selectComune.appendChild(opzione);
+    });
+})
+.catch((errore) => console.error("Comuni non caricati:", errore));
+
+/************************************************************
+ * GENERAZIONE CODICE FISCALE
+ ************************************************************/
+const formCF = document.getElementById("formCF");
+const outputCF = document.getElementById("outputCF");
+const risultatoCF = document.getElementById("risultatoCF");
+
+formCF.addEventListener("sumbit", (evento) => {
+    evento.preventDefault();
+
+    // Overlay Caricamento
+    document.getElementById("overlayCaricamento").classList.remove("nascosto");
+
+    // Salvo i dati dal form
+    const valoreCognome = document.getElementById("cognome").value.trim().toUpperCase();
+    const valoreNome = document.getElementById("nome").value.trim().toUpperCase();
+    const valoreDataNascita = document.getElementById("dataNscita").value;
+    const sessoSelezionato = document.getElementById('input[name="sesso":checked]');
+    const codiceComune = selectComune.value;
+})
